d731d67bacf402fdcaf4765915cf1446ea34631c
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-10-17 09:24:19 +0300

    changes

--- main.java
+++ main.java
@@ -17,6 +17,22 @@ public class main{
         frame.add(panel1, BorderLayout.NORTH);
         panel1.add(label1);
         panel1.setBackground(java.awt.Color.GRAY);
-        
-       
-}}
+        JButton button1 = new JButton("login");
+        button1.setBounds(140, 150, 100, 30);
+        frame.add(button1);
+        JTextField tf1 = new JTextField();
+        tf1.setBounds(140, 70, 100, 30);
+        JTextField tf2 = new JTextField();
+        tf2.setBounds(140,110,100,30);
+        frame.add(tf1);
+        frame.add(tf2);
+        frame.setResizable(true);
+        JLabel lab1 = new JLabel("Email/phone");
+        //lab1.setFont(new Font("arial", Font.ITALIC, 10));
+        lab1.setBounds(65, 75, 100, 30);
+        frame.add(lab1);
+        JLabel lab2 = new JLabel("password");
+        lab2.setBounds(70,105,100,30);
+        frame.add(lab2);
+    }
+}      
